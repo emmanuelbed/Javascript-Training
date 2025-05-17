b59fe2fa04541b5f73cd3489bc38f9f85dab65d1
bedzoemmanuel8@gmail.com <bedzoemmanuel8@gmail.com>
2025-05-17 11:33:59 -0500

    Applying else ifs...

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 let hasMembership = false
+let signedIn = true
 
 if(hasMembership === true){
     console.log("Show the video")
+}
+else if(signedIn === true){
+    console.log("Please upgrade your account")
 }
 else{
     console.log("Please log in")
