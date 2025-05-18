1784453d0d55ef3ddfd3774ce119022c631b444a
bedzoemmanuel8@gmail.com <bedzoemmanuel8@gmail.com>
2025-05-18 14:50:34 -0500

    Exercise on .map without using the .map method

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 /** Currency Converter
  * 
  * Turn the elements in an array from USD to AUD
+ * without using .map method
  * 
  * USD to AUD formular:
  * AUD = USD x 1.5
@@ -13,10 +14,11 @@
 
 let usd = [10, 20, 30, 40]
 
-let aud = usd.map((element) => {
-    return element * 1.5
-})
+let aud = []
 
+for (i = 0; i < usd.length; i++){
+    aud.push(usd[i] * 1.5 );
+}
 
 console.log(aud);
 
