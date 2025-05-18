c0a38e0b64815a10a390e933d4876e8bcbdc7831
bedzoemmanuel8@gmail.com <bedzoemmanuel8@gmail.com>
2025-05-18 13:08:56 -0500

    Working with Array Methods

--- app.js
+++ app.js
@@ -3,3 +3,18 @@ let item2 = "Oranges"
 let item3 = "Eggs"
 
 let array = ["Apples", "Bananas", "Oranges", "Eggs", "Milk"]
+// First Element
+console.log(array[0])
+
+// Last Element
+console.log(array[array.length - 1])
+
+// Array methods
+array.push("Bread")
+
+array.filter((element) = {}))
+
+
+
+console.log(array)
+
