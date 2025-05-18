30f1dfc0d2bfbc05efad72253e46804833751f21
bedzoemmanuel8@gmail.com <bedzoemmanuel8@gmail.com>
2025-05-18 12:14:28 -0500

    Second FOR LOOP example

--- app.js
+++ app.js
@@ -1,25 +1,19 @@
-/**  Write a for loop that loops through 1 to 15
- * If the number is odd, print "ASAP"
- * If the number is rven, print "Frontend"
- * If the number is divisible by 5, print "ASAP Frontend"
+/**  Print all the characters from the sring
+ * "ASAP Frontend"
  * 
  * 
  * @example
- * 1 - "ASAP"
- * 2 - "Frontend"
- * 3 - "ASAP"
- * 4 - "Fronend"
- * 5 - "ASAP Frontend"
+ * "A"
+ * "S"
+ * "A"
+ * 
+ * 
  * 
 */
- for (let i = 1; i <= 15; i++){
-    if(i % 5 === 0){
-        console.log(`${i} - ASAP Frontend`);
-    }
-    else if(i % 2 === 0){
-        console.log(`${i} - Frontend`);
-    }
-    else{
-        console.log(`${i} - ASAP`);
-    }
+
+let word = "ASAP Frontend"
+
+
+ for (let i = 0; i < word.length; i++){
+    console.log(`"${word[i]}"`)
  }
