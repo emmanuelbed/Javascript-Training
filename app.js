5472eb608881edd65f29aa53290eca5755a6b27c
bedzoemmanuel8@gmail.com <bedzoemmanuel8@gmail.com>
2025-05-18 14:39:33 -0500

    Understanding the .map methos

--- app.js
+++ app.js
@@ -1,23 +1,9 @@
-/** Night Club Exercise
- * 
- * Filter out all the people wo are not 18 years old
- * 
- * @example
- * [18, 20, 14, 15, 21] -> [18, 20, 21]
- */
+let array = [1, 2, 3, 4, 5]
+// Changes the original values of the array based on what is returned
+// NB it is a non-mutationg methd.
+let newArray = array.map((element) => {
+    console.log(element);
+    return null
+})
 
-
-
-let people = [18, 20, 14, 15, 21] 
-
-let allowedPeople = []
-
-for(i = 0; i< people.length; i++){
-    if (people[i] >= 18){
-        allowedPeople.push(people[i])
-    }
-}
-
-console.log(allowedPeople);
-
-
+console.log(newArray);
