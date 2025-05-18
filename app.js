59a34559972ac0f86e0ebe3862408000a12a6db0
bedzoemmanuel8@gmail.com <bedzoemmanuel8@gmail.com>
2025-05-18 13:31:02 -0500

    Night club age checker

--- app.js
+++ app.js
@@ -1,21 +1,17 @@
+/** Night Club Exercise
+ * 
+ * Filter out all the people wo are not 18 years old
+ * 
+ * @example
+ * [18, 20, 14, 15, 21] -> [18, 20, 21]
+ */
 
 
-let array = [20, 15, 10, 30]
 
+let array = [18, 20, 14, 15, 21] 
 
-// ["Apples", "Bananas", "Oranges", "Eggs", "Milk"]
-
-let filteredArray = array.filter((element) => {
-    
-    if(element >= 15){
-        return true
-    }
-    else{
-        return false
-    }
+let filterredArray = array.filter((element) => {
+    return element >= 18
 })
 
-
-
-console.log(filteredArray)
-
+console.log(filterredArray)
