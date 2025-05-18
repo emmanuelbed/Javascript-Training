ff618f3c8f594131a0495b10502ccbc1f86f86c9
bedzoemmanuel8@gmail.com <bedzoemmanuel8@gmail.com>
2025-05-18 12:05:04 -0500

    Looping example excersise

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
-for (let i = 0; i < 10; i++){
-    console.log(i)
-}
+/**  Write a for loop that loops through 1 to 15
+ * If the number is odd, print "ASAP"
+ * If the number is rven, print "Frontend"
+ * If the number is divisible by 5, print "ASAP Frontend"
+ * 
+ * 
+ * @example
+ * 1 - "ASAP"
+ * 2 - "Frontend"
+ * 3 - "ASAP"
+ * 4 - "Fronend"
+ * 5 - "ASAP Frontend"
+ * 
+*/
+ for (let i = 1; i <= 15; i++){
+    if(i % 5 === 0){
+        console.log(`${i} - ASAP Frontend`);
+    }
+    else if(i % 2 === 0){
+        console.log(`${i} - Frontend`);
+    }
+    else{
+        console.log(`${i} - ASAP`);
+    }
+ }
